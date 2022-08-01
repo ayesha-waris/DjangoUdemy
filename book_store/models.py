4294f37cbf6f7fc89347ff59b3c491527b788db8
ayeshoali <ayeshoali@gmail.com>
2022-08-01 21:21:48 +0500

    reverse method added in model

--- book_store/models.py
+++ book_store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.urls import reverse
 # Create your models here.
 
 
@@ -11,5 +12,9 @@ class Book(models.Model):
     is_bestselling = models.BooleanField(default=False)
 
     # add methods
+
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[self.id])
+
     def __str__(self):
         return f"{self.title} ({self.rating})"
