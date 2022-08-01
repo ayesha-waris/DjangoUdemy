f0e3e26b324952e4d35214164ef22e7ee052be7e
ayeshoali <ayeshoali@gmail.com>
2022-08-01 15:57:36 +0500

    model introduction along with migration

---
+++ book_store/models.py
@@ -0,0 +1,9 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+
+
+class Book(models.Model):
+     title = models.CharField(max_length=50)
+     rating = models.IntegerField()
