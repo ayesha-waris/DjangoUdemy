21cc12de01426eb5a9331f6fad9024a679170b2a
ayeshoali <ayeshoali@gmail.com>
2022-08-02 14:15:15 +0500

    configured model fields

--- book_store/models.py
+++ book_store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
@@ -8,10 +8,10 @@ from django.utils.text import slugify
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
-        validators=[MinLengthValidator(1), MaxLengthValidator(5)])
+      validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="",editable=False, blank=True, null=False, db_index=True)
     # add methods
 
     def get_absolute_url(self):
