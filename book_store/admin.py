cdeabe44bdad1fce72fc96c978b47be41d3a0251
ayeshoali <ayeshoali@gmail.com>
2022-08-02 16:55:35 +0500

    Address model created and made one-to-one relation with Author also added MEta class in Admin Configuration

--- book_store/admin.py
+++ book_store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Author, Book
+from .models import Address, Author, Book
 
 # Register your models here.
 
@@ -12,6 +12,10 @@ class BookAdmin(admin.ModelAdmin):
 class AuthorAdmin(admin.ModelAdmin):
   list_display = ("first_name","last_name")
 
+class AddressAdmin(admin.ModelAdmin):
+  list_display = ("street","city")
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author,AuthorAdmin)
+admin.site.register(Address,AddressAdmin)
 
