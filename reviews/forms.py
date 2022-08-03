cdd0cdac56df9a061189738d566584d38585f872
ayeshoali <ayeshoali@gmail.com>
2022-08-03 15:15:07 +0500

    styling added to form

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-  user_name = forms.CharField()
+  user_name = forms.CharField(label="User Name",required=True, max_length=50, error_messages={
+    "required": "Please enter your name"
+  })
  
 
--- reviews/views.py
+++ reviews/views.py
@@ -7,19 +7,20 @@ from .forms import ReviewForm
 
 
 def review(request):
- if request.method == 'POST':
+  if request.method == 'POST':
     form = ReviewForm(request.POST)
 
     if form.is_valid():
       print(form.cleaned_data)
-      return HttpResponseRedirect('/thank-you')
-    
+      return HttpResponseRedirect("/thank-you")
 
- form = ReviewForm()
+  else:
+    form = ReviewForm()
 
- return render(request, "reviews/review.html", {
-        "form": form
-    })
+  return render(request, "reviews/review.html", {
+    "form": form
+  })
+
 
 
 def thank_you(request):
