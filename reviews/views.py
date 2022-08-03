265ed224c5a7b1c4c1ea0cc53c64d16b917dce27
ayeshoali <ayeshoali@gmail.com>
2022-08-03 13:58:25 +0500

    extracting data from POST request

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,18 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
 
 # Create your views here.
 
 
 def review(request):
+  if request.method == "POST":
+    entered_name = request.POST['name']
+    print(entered_name)
+    return HttpResponseRedirect('/thank-you')
+
   return render(request, "reviews/review.html")
+
+
+def thank_you(request):
+  return render(request,'reviews/thank_you.html')
