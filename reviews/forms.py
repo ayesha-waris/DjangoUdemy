21a202150fea3f987abbb726edd167e347573948
ayeshoali <ayeshoali@gmail.com>
2022-08-03 15:27:27 +0500

    added more form controls

--- reviews/forms.py
+++ reviews/forms.py
@@ -4,5 +4,7 @@ class ReviewForm(forms.Form):
   user_name = forms.CharField(label="User Name",required=True, max_length=50, error_messages={
     "required": "Please enter your name"
   })
+  review_text = forms.CharField(label="Feedback",widget=forms.Textarea, max_length=200 )
+  rating = forms.IntegerField(label="Rating", min_value=1, max_value=5)
  
 
