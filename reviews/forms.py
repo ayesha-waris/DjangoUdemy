27d0f1d5731eb8003d8e52ee87d24038b02e9550
ayeshoali <ayeshoali@gmail.com>
2022-08-03 15:52:37 +0500

    added modelform

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,10 +1,16 @@
+from dataclasses import field
 from django import forms
 
-class ReviewForm(forms.Form):
-  user_name = forms.CharField(label="User Name",required=True, max_length=50, error_messages={
-    "required": "Please enter your name"
-  })
-  review_text = forms.CharField(label="Feedback",widget=forms.Textarea, max_length=200 )
-  rating = forms.IntegerField(label="Rating", min_value=1, max_value=5)
- 
+from reviews.models import Review
 
+# class ReviewForm(forms.Form):
+#   user_name = forms.CharField(label="User Name",required=True, max_length=50, error_messages={
+#     "required": "Please enter your name"
+#   })
+#   review_text = forms.CharField(label="Feedback",widget=forms.Textarea, max_length=200 )
+#   rating = forms.IntegerField(label="Rating", min_value=1, max_value=5)
+ 
+class ReviewForm(forms.ModelForm):
+  class Meta:
+    model = Review
+    fields = '__all__'
