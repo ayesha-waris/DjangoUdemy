6e9403519b3a8ee69259f0c445245a0c9926db69
ayeshoali <ayeshoali@gmail.com>
2022-08-03 22:02:16 +0500

    formView added

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from .forms import ReviewForm
 from .models import Review
@@ -10,23 +11,31 @@ from .models import Review
 # Create your views here.
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm(request.POST)
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
+    # def get(self, request):
+    #     form = ReviewForm(request.POST)
+    #     return render(request, "reviews/review.html", {
+    #         "form": form
+    #     })
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+    # def post(self, request):
+    #     form = ReviewForm(request.POST)
+
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect("/thank-you")
+
+    #     return render(request, "reviews/review.html", {
+    #         "form": form
+    #     })
 
 # def review(request):
 #   if request.method == 'POST':
@@ -63,9 +72,9 @@ class ThankYouView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["message"] = "Hii" 
+        context["message"] = "Hii"
         return context
-    
+
 
 # class ReviewsListView(TemplateView):
 #     template_name = "reviews/review_list.html"
@@ -77,22 +86,16 @@ class ThankYouView(TemplateView):
 
 
 class ReviewsListView(ListView):
-    template_name = "reviews/review_list.html" 
-    model = Review 
+    template_name = "reviews/review_list.html"
+    model = Review
     context_object_name = "reviews"
 
     # def get_queryset(self):
     #     base_query = super().get_queryset()
     #     data = base_query.filter(rating__gt=1)
     #     return data
-    
 
-    
 
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
-
-    
-
-
