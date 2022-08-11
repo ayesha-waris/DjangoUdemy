5337b1b22dc779786c2916ca9feed063bf32f804
ayeshoali <ayeshoali@gmail.com>
2022-08-11 17:32:10 +0500

    staff, admin, regular users understanding

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
@@ -7,6 +7,7 @@ urlpatterns = [
   path("view_blog/<int:blog_id>/", views.view_blog, name="view_blog"),
   path("see_request/", views.see_request),
   path("user_info/", views.user_info),
-  path("private_place/", views.private_place)
+  path("private_place/", views.private_place),
+  path("accounts/", include("django.contrib.auth.urls")),
 ]
   
